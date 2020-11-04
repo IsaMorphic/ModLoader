7f781eb1469da699f99f75ae5ab23452ccc9027a
Yodadude2003 <yodadude2003@gmail.com>
2020-11-04 15:13:51 -0600

    Change version number

--- ModLoader/AboutForm.Designer.cs
+++ ModLoader/AboutForm.Designer.cs
@@ -60,7 +60,7 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(624, 50);
             this.label1.TabIndex = 1;
-            this.label1.Text = "ModLoader v1.7";
+            this.label1.Text = "ModLoader v1.8";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label2
@@ -126,7 +126,7 @@
             // label8
             // 
             this.label8.Font = new System.Drawing.Font("Lucida Sans", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(12, 288);
+            this.label8.Location = new System.Drawing.Point(14, 288);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(624, 18);
             this.label8.TabIndex = 8;
